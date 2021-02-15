485c84c7285930be9b13d48de66c2b73387c4281
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-02-15 14:25:53 +0100

    LabelHandler with a subset of the properties (Text and TextColor)

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Label/LabelHandler.cs
@@ -1,39 +1,24 @@
-﻿using System;
-#if __IOS__
-using NativeView = UIKit.UILabel;
-#elif __MACOS__
-using NativeView = AppKit.NSTextField;
-#elif MONOANDROID
-using NativeView = Android.Widget.TextView;
-#elif NETCOREAPP
-using NativeView = System.Windows.Controls.TextBlock;
-#elif NETSTANDARD
-using NativeView = System.Object;
-#endif
-
-namespace Xamarin.Platform.Handlers
+﻿namespace Xamarin.Platform.Handlers
 {
-	public class LabelHandler : AbstractViewHandler<ILabel, NativeView>
+	public partial class LabelHandler
 	{
 		public static PropertyMapper<ILabel, LabelHandler> LabelMapper = new PropertyMapper<ILabel, LabelHandler>(ViewHandler.ViewMapper)
 		{
-			[nameof(ILabel.Color)] = MapColor,
-			[nameof(ILabel.Text)] = MapText,
+			[nameof(IText.Text)] = MapText,
+			[nameof(IText.TextColor)] = MapTextColor,
+			[nameof(IText.Font)] = MapFont,
+			[nameof(IText.TextTransform)] = MapTextTransform,
+			[nameof(IText.CharacterSpacing)] = MapCharacterSpacing,
+			[nameof(ILabel.LineHeight)] = MapLineHeight,
+			[nameof(ILabel.FontSize)] = MapFontSize,
+			[nameof(ILabel.FontAttributes)] = MapFontAttributes,
+			[nameof(ILabel.HorizontalTextAlignment)] = MapHorizontalTextAlignment,
+			[nameof(ILabel.VerticalTextAlignment)] = MapVerticalTextAlignment,
+			[nameof(ILabel.TextDecorations)] = MapTextDecorations,
+			[nameof(ILabel.LineBreakMode)] = MapLineBreakMode,
+			[nameof(ILabel.MaxLines)] = MapMaxLines,
+			[nameof(IPadding.Padding)] = MapPadding
 		};
-
-		public static void MapColor(LabelHandler handler, ILabel Label)
-		{
-		}
-
-		public static void MapText(LabelHandler handler, ILabel label)
-		{
-			handler.TypedNativeView?.UpdateText(label);
-		}
-#if MONOANDROID
-		protected override NativeView CreateNativeView() => new NativeView(this.Context);
-#else
-		protected override NativeView CreateNativeView() => new NativeView();
-#endif
 
 		public LabelHandler() : base(LabelMapper)
 		{
